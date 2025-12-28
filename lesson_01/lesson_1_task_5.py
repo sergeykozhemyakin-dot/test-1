5310041a0374feb1b8b960ddcd113b5d4e6596ba
Сергей Кожемякин <sergey.kozhemyakin@gmail.com>
2025-12-28 12:26:30 +0200

    Домашняя работа

---
+++ lesson_01/lesson_1_task_5.py
@@ -0,0 +1,14 @@
+def print_num():
+    print(8, end='')
+    print(8, end='')
+    print(0, end='')
+    print(0, end='')
+    print(5, end='')
+    print(5, end='')
+    print(5, end='')
+    print(3, end='')
+    print(5, end='')
+    print(3, end='')
+    print(5, end='')
+print_num()
+
